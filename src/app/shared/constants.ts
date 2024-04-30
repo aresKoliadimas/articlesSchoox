3545163cbc7b7b7070525e20b65fdee75ba90c0f
Ares Koliadimas <ariskoliadimas@gmail.com>
2024-04-30 15:25:03 +0300

    Create constants

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,12 @@
+export const SPACES_SYMBOLS_REGEX: RegExp = /\s+|[^a-zA-Z0-9]+/;
+export const WHITESPACE: string = ' ';
+export const SEARCH_TEXT: string = 'Search...';
+export const SEARCH_TOOLTIP_TEXT: string =
+  'Search a title, author or a word in article body';
+export const NO_ARTICLES_PLACEHOLDER_TEXT: string =
+  'The term you searched produced no results, please search another term.';
+export const BY_TEXT: string = 'by ';
+export const CREATED_ON_TEXT: string = 'created on: ';
+export const UPDATED_ON_TEXT: string = 'updated on: ';
+export const UP_ARROW_TEXT: string = '\u2B9D';
+export const DOWN_ARROW_TEXT: string = '\u2B9F';
